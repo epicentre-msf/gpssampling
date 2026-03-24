d3981eb2372954c6d56cb432f322accfbada7dd8
Yves Amevoin <amevoina@yahoo.fr>
2026-03-24 08:01:46 +0300

    Foced non parallel test on macos

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,7 +1,1 @@
 # styler: block
-
-# Disable parallel testthat on macOS: forked processes + dyn.load()
-# of GDAL native libraries causes segfaults on macOS ARM64 CI runners.
-if (Sys.info()[["sysname"]] == "Darwin") {
-  options(testthat.parallel = FALSE) # nolint
-}
